3df5c60a89492a2ea04ecc097e2dba0411853e9a
Royal Davanam <royaldavanam02@gmail.com>
2025-12-23 17:51:31 +0530

    Added Loops Practice

---
+++ Java/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 8;
+        if (i < j) {
+            System.out.println("I is smaller than the J");
+        }
+        else {
+            System.out.println("J is lager than I");
+        }
+    }
+}
