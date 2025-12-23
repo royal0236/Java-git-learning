8b758ea0fcbbeb73e5881aefadb3badc2104e0d8
Royal Davanam <royaldavanam02@gmail.com>
2025-12-23 17:26:18 +0530

    Add java intial practiced basic problems

---
+++ Java/Hello.java
@@ -0,0 +1,53 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        // System.out.println("DAVANAM VENKATA SOWMYANADH ROYAL");
+        // System.out.println("Hemanth");
+        //Create a variable to store your age and print it.
+        // int age = 20;
+        // System.out.println("My age is:" + age);
+        //Create two variables to store two numbers and print their sum.
+        // int i = 20;
+        // int j = 22;
+        // System.out.println(i + j);
+        // Store your marks in 3 subjects in 3 variables and print the total marks.
+        // int Subject1 = 89;
+        // int Subject2 = 97;
+        // int Subject3 = 95;
+        // System.out.println("Total Marks Obatined for 300/"+(Subject1+Subject2+Subject3));
+        // Create a variable for year of birth and calculate your current age.  
+        // int year_of_birth = 2005;
+        // System.out.println("My current age is:" + (2025 - year_of_birth));
+       // Store length and breadth of a rectangle in variables and print: Area , Perimeter
+    //    int length = 3;
+    //    int breadth = 10;
+    //    System.out.println("Area of the rectangle is:" + (length * breadth) + "\n"
+    //            + "Perimeter of the rectangle is:" + (2 * (length + breadth)));
+    // Declare variables for:your age , your grade , whether you are a student. Print all.
+    // int age = 20;
+    // char grade = 'A';
+    // boolean Student = true;
+    // System.out.println(
+    //         "My age is:" + age + "\n" + "My grade is:" + grade + "\n" + "I am Student:" + Student);
+    // // Store price of a product and print it
+    // double product = 99.99;
+    // System.out.println("The price of a Product is:" + product);
+    // // Store marks of 5 subjects using different data types and print total.
+    // int Subject1 = 89;
+    // float Subject2 = 99f;
+    // long Subject3 = 97L;
+    // double Subject4 = 94;
+    // short Subject5 = 91;
+    // System.out.println(
+    //         "Marks of different subjects:" + "\n" + Subject1 + "\n" + Subject2 + "\n"+ Subject3 + "\n"+ Subject4 + "\n"+ Subject5);
+    // Git Level 2: learning how to see changes
+
+ 
+
+
+
+
+
+
+    }
+}
